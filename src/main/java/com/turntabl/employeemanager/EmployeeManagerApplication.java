c2256c735358e669ab9a81e3a5fa775c120b36d1
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2022-01-15 16:09:10 +0000

    fixed cross-origin issues and tested the end points with angular

--- src/main/java/com/turntabl/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/turntabl/employeemanager/EmployeeManagerApplication.java
@@ -1,14 +1,9 @@
 package com.turntabl.employeemanager;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeeManagerApplication {
