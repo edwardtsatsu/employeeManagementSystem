abeb7d4133de54a7bc8cd7dfea5d45859e1903cc
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2022-01-15 13:13:44 +0000

    fixed cross-origin issues

--- src/main/java/com/turntabl/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/turntabl/employeemanager/EmployeeManagerApplication.java
@@ -1,13 +1,31 @@
 package com.turntabl.employeemanager;
-
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeeManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("HEAD","GET","POST","PUT","DELETE","PATCH","OPTIONS").allowedOrigins("http://localhost:4200");
+
+			}
+		};
 	}
 
 }
